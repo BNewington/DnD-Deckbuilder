b980f688b086cf2780c3c819d5038ad2b9728def
Benjamin Newington <benjaminanewington@gmail.com>
2026-02-21 14:03:50 +0000

    increased card aiming height

--- scenes/card_ui/card_state/card_aiming_state.gd
+++ scenes/card_ui/card_state/card_aiming_state.gd
@@ -9,7 +9,7 @@ func enter() -> void:
 	card_ui.color.color = Color.WEB_PURPLE
 	card_ui.state.text = "AIMING"
 	
-	card_ui.animate_to_position(card_ui.hand.global_position - Vector2(0,20), 0.2)
+	card_ui.animate_to_position(card_ui.hand.global_position - Vector2(0,40), 0.2)
 	card_ui.targets.clear()
 	
 	var hero_pos = Navigation.get_tile_coords(card_ui.belongs_to.global_position)
