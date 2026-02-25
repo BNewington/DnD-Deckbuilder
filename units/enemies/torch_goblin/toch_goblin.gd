e058816527a26db9f63f9772992f0ef418aeac8c
Benjamin Newington <benjaminanewington@gmail.com>
2026-02-25 19:25:59 +0000

    updated pathfinding

--- units/enemies/torch_goblin/toch_goblin.gd
+++ units/enemies/torch_goblin/toch_goblin.gd
@@ -7,7 +7,7 @@ func start_turn(unit: Unit):
 	var pos = Navigation.get_tile_coords(unit.global_position)
 	var target_tiles = find_target_tiles(unit)
 	print("target tiles: ",target_tiles)
-	if target_tiles.size() > 0:
+	if target_tiles.size() > 0 and not pos in target_tiles:
 		unit.move_to(target_tiles[0])
 	await unit.get_tree().create_timer(1).timeout
 	print(name," turn over")
