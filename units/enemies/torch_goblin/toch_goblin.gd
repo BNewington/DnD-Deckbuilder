9a3bbde88e9604f47701ebbd3aef4b3d491f7676
Benjamin Newington <benjaminanewington@gmail.com>
2026-02-25 18:21:41 +0000

    fixed pathfinding bug

--- units/enemies/torch_goblin/toch_goblin.gd
+++ units/enemies/torch_goblin/toch_goblin.gd
@@ -7,7 +7,8 @@ func start_turn(unit: Unit):
 	var pos = Navigation.get_tile_coords(unit.global_position)
 	var target_tiles = find_target_tiles(unit)
 	print("target tiles: ",target_tiles)
-	unit.move_to(target_tiles[0])
+	if target_tiles.size() > 0:
+		unit.move_to(target_tiles[0])
 	await unit.get_tree().create_timer(1).timeout
 	print(name," turn over")
 	Events.turn_ended.emit(unit)
